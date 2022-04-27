8501f7bb0742edcaeecb3b4f81bbe2dfca01448d
Kim Dill-McFarland <kdillmcfarland@gmail.com>
2022-04-26 18:04:28 -0700

    Add RNAseq data

---
+++ 0_data/raw_data_cleaning.R
@@ -0,0 +1,55 @@
+#### Setup ####
+library(tidyverse)
+library(stringr)
+
+meta <- read_csv("0_data/2020.11.20RSTR_Hawn_metadata.csv")
+count <- read_csv("0_data/Hawn_UgandaMacrophage_counts.csv") %>% 
+  rename("hgnc_symbol"=`...1`)
+
+#### Format data ####
+#List samples with RNAseq data
+samp <- data.frame(libID_orig=colnames(count)) %>% 
+  separate(libID_orig, into=c("RS_SUB_ACCESSION_NO","condition"), sep="_",
+           remove=FALSE) %>% 
+  mutate(condition = recode(condition, "TB"="Mtb","MEDIA"="Media"))
+
+meta.ltbi <- meta %>% 
+  #Filter LTBI samples with RNAseq data
+  filter(Sample_Group == "LTBI" & 
+           RS_SUB_ACCESSION_NO %in% samp$RS_SUB_ACCESSION_NO) %>% 
+  #Select variables of interest
+  select(FULLIDNO, RS_SUB_ACCESSION_NO, M0_KCVSEX, 
+         KCHCA_AGE_YR_CURRENT, avgBMI, RISK_SCORE) %>% 
+  #Add library ID for MEDIA and TB
+  inner_join(samp) %>% 
+  #rename to short names
+  rename(sex=M0_KCVSEX, age_yrs=KCHCA_AGE_YR_CURRENT, bmi=avgBMI,
+         risk=RISK_SCORE)
+
+# Select 10 random donors
+set.seed(8759)
+samp10 <- sample(unique(meta.ltbi$RS_SUB_ACCESSION_NO), size=10, 
+                 replace=FALSE)
+meta.ltbi10 <- meta.ltbi %>% 
+  filter(RS_SUB_ACCESSION_NO %in% samp10) %>% 
+  #De-identify
+  mutate(ptID = paste0("pt", 
+                       str_pad(as.numeric(factor(RS_SUB_ACCESSION_NO)),2,pad = "0")),
+         libID = paste(ptID, condition, sep="_")) 
+  
+
+#Filter and rename counts table
+count.ltbi <- count %>% 
+  pivot_longer(-hgnc_symbol, names_to = "libID_orig") %>% 
+  inner_join(select(meta.ltbi10, libID_orig, libID)) %>% 
+  select(-libID_orig) %>% 
+  pivot_wider(names_from = "libID")
+
+#### Save ####
+write_csv(count.ltbi, file="0_data/raw_counts.csv")
+
+meta.ltbi10 %>% 
+  select(libID, ptID, condition, age_yrs, sex, bmi, risk) %>% 
+  arrange(ptID, condition) %>% 
+  write_csv(file="0_data/metadata.csv")
+
